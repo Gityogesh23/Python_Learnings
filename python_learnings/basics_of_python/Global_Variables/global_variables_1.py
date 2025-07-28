1e4cdd42d945a8dd61b073e4c9208c1495507999
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-07-28 20:00:36 +0530

    commit the code

---
+++ python_learnings/basics_of_python/Global_Variables/global_variables_1.py
@@ -0,0 +1,19 @@
+# Global variables can be used by everyone, both inside of functions and outside
+# x = "awesome"
+#
+# def myfunc():
+#   print("Python is " + x)
+#
+# myfunc()
+
+# If you create a variable with the same name inside a function, this variable will be local,
+# and can only be used inside the function. The global variable with the same name will remain as it was, global and with the original value.
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
