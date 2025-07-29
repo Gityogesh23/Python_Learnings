0bd9d868f071464a293618f447aedeadf3ccb1eb
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-07-29 20:09:39 +0530

    commit the code

---
+++ python_learnings/basics_of_python/python_loops/python_while_1.py
@@ -0,0 +1,33 @@
+# WHILE LOOP-->
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+  # Note:Note: remember to increment i, or else the loop will continue forever.
+  # The while loop requires relevant variables to be ready, in this example we need to define an indexing variable, i, which we set to 1.
+
+#BREAK STATEMENT-->
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 3:
+#     break
+#   i += 1
+#
+  #CONTINUE STATEMENT
+
+# i = 0
+# while i < 6:
+#   i += 1
+#   if i == 3:
+#     continue
+#   print(i)
+
+# THE ELSE STATEMENT-->
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
