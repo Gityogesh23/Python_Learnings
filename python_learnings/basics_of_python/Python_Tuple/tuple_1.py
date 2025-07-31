992ed714b971d66f7e99361567f65fc5f5238b3a
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-07-31 20:09:43 +0530

    code commit

--- python_learnings/basics_of_python/Python_Tuple/tuple_1.py
+++ python_learnings/basics_of_python/Python_Tuple/tuple_1.py
@@ -1,3 +1,8 @@
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary,
+# all with different qualities and usage.
+#
+# A tuple is a collection which is ordered and unchangeable.
+
 # thistuple = ("apple", "banana", "cherry")
 # print(thistuple)
 
@@ -41,11 +46,26 @@
 # It is also possible to use the tuple() constructor to make a tuple.
 # Using the tuple() method to make a tuple:
 
-thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
-print(thistuple)
+# thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thistuple)
 
 ## Access Tuples in Python -->in python negave indexing also works
 # consider above example
-print(thistuple[-2]);
-print(thistuple[0]);
-print(thistuple[-1]);
+# print(thistuple[-2]);
+# print(thistuple[0]);
+# print(thistuple[-1]);
+
+
+ #print tuples by giving an range -->#This will return the items from position 2 to 5.
+
+#Remember that the first item is position 0,
+#and note that the item in position 5 is NOT included
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+print(thistuple[3:5])
+# giving only first 5th items names excluding 5th -->i.e melon
+print(thistuple[:5])
+
+# By leaving out the end value, the range will go on to the end of the tuple: giving all from "cherry" till end..
+print(thistuple[2:])
