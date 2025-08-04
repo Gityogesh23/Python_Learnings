fb13f5d95d76edeab247f2d5308fc27720d8b5ad
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-08-04 20:14:11 +0530

    save code

--- python_learnings/basics_of_python/Python_Tuple/unpack_tuple_1.py
+++ python_learnings/basics_of_python/Python_Tuple/unpack_tuple_1.py
@@ -1,1 +1,10 @@
 # Python - Unpack Tuples -->Unpacking a Tuple
+# Note: The number of variables must match the number of values in the tuple, if not, you must use an asterisk to
+# collect the remaining values as a list.
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
