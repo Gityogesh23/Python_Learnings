341ff7b1eee28ababb7dd04926de773f1ac5f3b4
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-08-11 20:03:59 +0530

    coomit todays_code

---
+++ python_learnings/basics_of_python/python_zero_to_hero/python_Iv_question/find_square_root.py
@@ -0,0 +1,8 @@
+import math
+
+list_of_numbers=(4,9,36,64,81,144,121,324); #list of numbers
+
+# Calculate square root of numbers and store in a list
+
+square_root=[math.sqrt(num) for num in list_of_numbers];
+print(square_root);
