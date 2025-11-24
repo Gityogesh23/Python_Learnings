bdd336b83380be731a772d14c5a30965f9d2d827
Yogesh <yogesh@Yogesh-Laptop.localdomain>
2025-11-24 13:23:12 +0000

    commit of the day

--- Day_2/fun_utils.py
+++ Day_2/fun_utils.py
@@ -1,14 +1,14 @@
 import os
 
-command="uptime"#load avg
+command="uptime"#load avgy
 commannd="date" #date
 
 def check_cpu(command): #defining a function
-    print(os.system(command))
+    return os.system(command)
 def check_date(command): #defining a function
     print(os.system(command))
 def check_ram(command):
     print(os.system(command))
 
-#check_ram("free -h") #to check RAM usage on Ubuntu
+check_ram("free -h") #to check RAM usage on Ubuntu
 check_ram("sudo dmidecode -t memory") #on windows -->To check RAM
