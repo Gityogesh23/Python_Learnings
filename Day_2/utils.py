27916f59c055ec0d2a3e20696e881bf36083e65f
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-11-25 18:51:26 +0530

    commit for now

--- Day_2/utils.py
+++
@@ -1,8 +0,0 @@
-import os #importing a new library into your code.wsl
-
-
-print(os.system('df -h')) #linux or mac
-print(os.system('systeminfo')) #to show system info on windows
-#print(os.system('htop')) #live RAM Usage monitor for better -->to see->htop (Linux)
-print(os.system('sysinfo')) #Windows
-
