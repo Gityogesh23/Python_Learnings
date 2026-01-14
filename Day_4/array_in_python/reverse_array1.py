c1eedac9c85b035665b2914b230f33d17d3f94ac
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-01-14 19:59:59 +0530

    commit todays pyhton array demo

---
+++ Day_4/array_in_python/reverse_array1.py
@@ -0,0 +1,13 @@
+# t]To reverse an list inside Array
+
+a= ["Pritam","Priya","Nidhi","Akshada","Ananya","Arav","Anant","Ajit"]
+print("PPrint Array Elements are :")
+
+'''for i in a:
+    print(i)
+    o/p:on newline so refer best and simple next method for onle line o/p
+'''
+print(a)
+print("Print array elements in reverse order")
+a= a[::-1]
+print(a)
