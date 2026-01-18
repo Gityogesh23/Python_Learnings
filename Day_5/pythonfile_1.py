37f3382c7a152a823a60b9fddbe39ebfc1053912
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-01-18 11:53:20 +0530

    commit basics of python

---
+++ Day_5/pythonfile_1.py
@@ -0,0 +1,32 @@
+
+tuple=(12,13,14,124,"yogesh")
+print(type(tuple))  #<class 'tuple'>
+print()
+print(tuple)
+
+#print(tuple() vs List[])
+print("tuple :")
+tuple =(12,13,14,15,16,20)
+print("type of class :",type(tuple),"or",type(tuple).__name__)
+
+print("List:")
+employees=["Yogesh","Kiran","Jayashree","Karishma"]
+print("List Of Employees Becomes : ",employees)
+
+#reversed List of employees and tuple thenafter
+print("Reversed List Of Employees Becomes: ")
+print(employees[::-1]) #step operator ::
+print("First in the List Of Employees: ")
+print(employees[:1])
+print("Fetch Last in the List Of Employees: ")
+
+print("Fetch Employee by Letter: ","Y :")
+
+
+print("Find by Name of Emplyee from Empployees List:")
+'''
+print("List of employees print Using For loop: ")
+for employee in employees:
+    print(employee)
+
+'''
